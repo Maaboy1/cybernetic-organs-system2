8a3539ac2fb393855e42db4f814cc5385b48afaa
abyan <abyan.huq@outlook.com>
2024-10-01 22:51:57 -0500

    Assignment 5 done

--- src/main/java/com/cybernetic/OrganInventory.java
+++ src/main/java/com/cybernetic/OrganInventory.java
@@ -21,11 +21,68 @@ public class OrganInventory {
 
     //ability to sort by multiple properties in order. name, model, compatibility using built-in sort
     public List<Organ> sortOrganByNameModelAndCompatibilityUsingBuiltInSort() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        List<Organ> sortedList = new ArrayList<>(inventory);
+        Collections.sort(sortedList, (o1, o2) -> {
+            int nameCompare = o1.getName().compareTo(o2.getName());
+            if (nameCompare != 0) {
+                return nameCompare;
+            }
+            int modelCompare = o1.getModel().compareTo(o2.getModel());
+            if (modelCompare != 0) {
+                return modelCompare;
+            }
+            return o1.getCompatibility().compareTo(o2.getCompatibility());
+        });
+        return sortedList;
     }
 
     //ability to sort by multiple properties in order. name, model, compatibility using quicksort
     public List<Organ> quickSortOrganByNameModelAndCompatibility(List<Organ> unmodifiableOrganList) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        List<Organ> modifiableList = new ArrayList<>(unmodifiableOrganList);
+        quickSort(modifiableList, 0, modifiableList.size() - 1);
+        return modifiableList;
+    }
+
+    private void quickSort(List<Organ> organList, int low, int high) {
+    if (low < high) {
+        int pi = partition(organList, low, high);
+        quickSort(organList, low, pi - 1);  // Sort left half
+        quickSort(organList, pi + 1, high); // Sort right half
     }
 }
+
+    private int partition(List<Organ> organList, int low, int high) {
+    Organ pivot = organList.get(high); // Taking the last element as pivot
+    int i = low - 1; // Pointer for the greater element
+
+    for (int j = low; j < high; j++) {
+        if (compareOrgan(organList.get(j), pivot) <= 0) {
+            i++;
+            swap(organList, i, j);
+        }
+    }
+    swap(organList, i + 1, high);
+    return i + 1;
+}
+
+    private int compareOrgan(Organ o1, Organ o2) {
+    int nameCompare = o1.getName().compareTo(o2.getName());
+    if (nameCompare != 0) {
+        return nameCompare;
+    }
+    int modelCompare = o1.getModel().compareTo(o2.getModel());
+    if (modelCompare != 0) {
+        return modelCompare;
+    }
+    return o1.getCompatibility().compareTo(o2.getCompatibility());
+}
+
+    private void swap(List<Organ> organList, int i, int j) {
+    Organ temp = organList.get(i);
+    organList.set(i, organList.get(j));
+    organList.set(j, temp);
+}
+
+    ;
+    }
+
